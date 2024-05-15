59ec92e39dc11f836df0a96259be40649a033d79
ColletSimon <153361329+ssaumon@users.noreply.github.com>
2024-05-15 10:31:28 +0200

    ajout de la charte graphique de l'intranet

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap 5 Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body style="background-color:#CEF1F9;">
+
+
+
+
+<div style="background-color:#03045F;" class=" p-3 text-white">
+  <h1 style="color:#EEE6D8;">galetezen</h1>
+  <p style="color:#EEE6D8;"></p>
+</div>
+
+<nav style="background-color:#0078B8;" class="navbar navbar-expand-sm">
+  
+          <div class="container-fluid">
+            <!-- Links -->
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link text-white" href="index.php">Accueil</a>
+              </li>
+              <li class="nav-item ">
+                <a class="nav-link text-white" href="#">Inscription</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Membres</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Moderation</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Fournisseurs</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Clients</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Fichiers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link text-white" href="#">Wiki</a>
+              </li>
+              
+            
+            </ul>
+            </div>
+        </nav>
+
+  <div>
+
+
+  <br><br><br><br><br><br><br><br><br><br><br>
+  </div>
+
+
+  <footer class="footer mb-0" style="background-color: #0078B8;">
+    <div class="container-fluid jumbotron " style="background-color: #0078B8; ">
+      <div class="row justify-content-center"> 
+        <div class="col text-white m-5">
+          nous contacter : <br>
+          mail : contact@galetezen.com <br>
+          telephone : 0299123456
+
+        </div>
+        <div class="col m-5">
+          <a href="nous.html">A Propos</a> <br>
+          <a href="aide.html"> Aide</a>
+
+
+        </div>
+      </div>
+    </div>
+  </footer>
+
+</body>
+</html>
