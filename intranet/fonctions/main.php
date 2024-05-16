baea2e92b6f4da728fcfefe19a15b5bb89a725db
TeoHubert <teohubert00@gmail.com>
2024-05-16 10:17:30 +0200

    modified: Correction de bug

--- intranet/fonctions/main.php
+++ intranet/fonctions/main.php
@@ -2,6 +2,6 @@
 echo "test";
 #Inclusion des différents modulue avec la fonction require_once
 session_start();
-require_once('connexion.php')
+require_once('connexion.php');
 require_once('role.php');
 ?>
