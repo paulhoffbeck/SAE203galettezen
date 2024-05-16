064d522259c3356c9c2409cf1669e08f80972227
ColletSimon <simoncollet2005@gmail.com>
2024-05-16 09:32:30 +0200

    ajout du index.php de intranet

--- intranet/fonctions/main.php
+++ intranet/fonctions/main.php
@@ -1,4 +1,5 @@
 <?php
 #Inclusion des différents modulue avec la fonction require_once
-require_once('connexion.php')
+require_once('connexion.php');
+require_once('headnavfoot.php');
 ?>
--- intranet/index.php
+++ intranet/index.php
@@ -1,1 +1,25 @@
 
+<?php
+
+require_once ("fonctions/main.php");
+head();
+navbar();
+
+
+?>
+
+  
+
+
+  <br><br><br><br><br><br><br><br><br><br><br>
+  </div>
+
+
+<?php
+
+footer();
+
+?>
+
+</body>
+</html>
