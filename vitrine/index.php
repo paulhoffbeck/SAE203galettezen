b4b3dc4ca09609031c11b32e1e4c6f141373b3a4
paulhoffbeck <paul.hoffbeck@etudiant.univ-rennes1.fr>
2024-05-16 12:11:33 +0200

    Update index.php

--- vitrine/index.php
+++ vitrine/index.php
@@ -3,7 +3,6 @@ include '../functions/functions.php'
 ?>
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
