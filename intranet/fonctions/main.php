1e9522cab37f68cbdacba3f57093476c884e403c
TeoHubert <teohubert00@gmail.com>
2024-05-16 10:15:54 +0200

    Base de gestion des rôles
    Ajout de la base de données de roles, de la page de gestion et du module de fonction

--- intranet/fonctions/main.php
+++ intranet/fonctions/main.php
@@ -1,4 +1,6 @@
 <?php
+echo "test";
 #Inclusion des différents modulue avec la fonction require_once
-require_once('connexion.php')
+// require_once('connexion.php');
+require_once('./role.php');
 ?>
