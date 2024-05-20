fedc81da42fd5de0cf2dd769c520dfb876e7f89e
paulhoffbeck <paul.hoffbeck@etudiant.univ-rennes1.fr>
2024-05-20 14:16:34 +0200

    Page d'acceuil

--- vitrine/index.php
+++ vitrine/index.php
@@ -8,19 +8,21 @@ include '../functions/functions.php'
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GaleteZen - Accueil</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
-    <link href="style.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 
 <body>
-    <div class="container-fluid">
-    <div class="row flex-nowrap">
+    <div class="row">
     <?php
-    afficher_sidebar();
+    sidebar();
     ?>
-    <div class="col-auto col-md-3 col-xl-2 px-0 bg-white">
-    
-</div>
-</div>
+    <h2 class="text-center">Nous sommes une entreprise malouine de crêpe bretonne que nous vendons à l'international</h2>
+    <p class="text-center"> Nous fabriquons nos crêpes avec des produits locaux ! Nous fabriquons des crêtes de qualité tout en soutenant l'agriculture locale</p>
+
+
+
+    </div>
     
 </body>
 
