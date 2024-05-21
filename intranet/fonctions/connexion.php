4473f0df742010ae5ec643b197b463dcdc7bedbb
MaxymeDes <maxime.despres.1@etudiant.univ-rennes1.fr>
2024-05-21 14:25:11 +0200

    modification d'erreur pour la connexion

--- intranet/fonctions/connexion.php
+++ intranet/fonctions/connexion.php
@@ -26,27 +26,28 @@ function connexion(){
         </div>
     </div>';
     if(isset($_POST['nom']) && isset($_POST['motdepasse'])){
-        $json = file_get_contents('./data/user.json');
+        $json = file_get_contents('./database/user.json');
         $donnee = json_decode($json, true);
 
         foreach($donnee as $user){
             if ($user['role'] != ""){
                 if($user['nom'] === $_POST['nom']){
-                    if(password_verify($_POST['motdepasse'], $user['motdepasse'])){
+                    //if(password_verify($_POST['motdepasse'], $user['motdepasse'])){
                         $_SESSION['iud'] = $user['iud'];
                         $_SESSION['nom'] = $user['nom'];
                         $_SESSION['prenom'] = $user['prenom'];
+                        $_SESSION['email'] = $user['email'];
                         $_SESSION['motdepasse'] = $user['motdepasse'];
                         $_SESSION['role'] = $user['role'];
                         echo "<a href='index.php'> Connexion validé</a>";
                         header("Refresh:0");
                         return;
-                    }
+                    /*}
                     else {
                         echo "Mot de passe incorrect.";              
-                    }
+                    }*/
                 }
-            }
+                }
             else{
                 echo "Votre rôle n'a pas été accepté par le responsable du service.";
             }
