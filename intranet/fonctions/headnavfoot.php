079ecd4f3cd541fca097ceaedb1181ab145937ca
TeoHubert <teohubert00@gmail.com>
2024-05-21 15:29:15 +0200

    [Intranet] Evolution head et navbar

--- intranet/fonctions/headnavfoot.php
+++ intranet/fonctions/headnavfoot.php
@@ -2,54 +2,59 @@
 
 function head(){
 echo'
-<div class="bg-pastel" class="p-3">
-  <img src="./img/logo.png" alt="Logo de GaletZen" class=" texte-center mr-3 rounded" 
-  style="max-width: 50px; max-height: 50px;">
-  <h1 class=" texte-center"> GaleteZen</h1>'; 
-  if(isset($_SESSION["prenom"])){
-    echo '<span class="text-indigo">Bonjour '.$_SESSION["prenom"].' '. $_SESSION["nom"]. ' !</span>
-        <a href="deconnexion.php" class="btn btn-outline-dark btn-sm">Deconnexion</a>';}
-  echo'</div>';
+<div class="bg-azur p-3">
+  <div class="row">
+    <div class="col-auto">
+      <img src="./img/logo.png" alt="Logo de GaletZen" class=" texte-center mr-3 rounded" style="max-width: 50px; max-height: 50px;">
+    </div>
+    <div class="col-auto">
+      <h1>GaleteZen</h1>
+    </div>
+  </div>
+</div>';
 }
 
-function  navbar(){
-echo('
-<nav class="navbar bg-azur navbar-expand-sm">
-  
-          <div class="container-fluid">
-            <!-- Links -->
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link text-white" href="index.php">Accueil</a>
-              </li>
-              <li class="nav-item ">
-                <a class="nav-link text-white" href="inscription.php">Inscription</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Membres</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Moderation</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Fournisseurs</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Clients</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Fichiers</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link text-white" href="#">Wiki</a>
-              </li>
-              
-            
-            </ul>
-            </div>
-        </nav>
-');
-}
+function  navbar(){ ?>
+<nav class="navbar navbar-expand-lg navbar-light bg-azur">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="index.php">Accueil</a>
+        </li>
+        <li class="nav-item ">
+          <a class="nav-link text-white" href="inscription.php">Inscription</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Membres</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Moderation</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Fournisseurs</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Clients</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Fichiers</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Wiki</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+          <span class="mt-1 text-white">Bonjour Jean Marie Le Pen ! </span>
+          <a href="deconnexion.php" class="btn btn-outline-ciel btn-sm text-white">Deconnexion</a>
+      </form>
+    </div>
+  </div>
+</nav>
+<?php }
 
 function footer(){
 
