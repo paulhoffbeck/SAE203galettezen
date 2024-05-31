6e9b1be3a2b083e2e06a15dd69fedaac291e5876
ColletSimon <simoncollet2005@gmail.com>
2024-05-31 14:17:36 +0200

    supr partenaires dans intra car mauvais fichier

--- functions/partenaires.php
+++ functions/partenaires.php
@@ -11,11 +11,11 @@ function partenaires(){
     
     foreach($partTable as $key => $value){
         if ($value["montrer"]=="oui") {
-        echo("<div class=\"col-4 mb-4\">
+        echo("<div class=\"col-3 mb-4\">
                 <div class=\"card mb-4 d-flex h-100 flex-column\">
                     <img src=\"../intranet/img/parter/".$value["image"]."\" class=\"card-img-top img-fluid\" alt=\"\"\>
                     <div class=\"card-body\">
-                        <h5 class=\"card-title\">".$key."</h5>
+                        <h5 class=\"card-title border-top pt-2\">".$key."</h5>
                         <p class=\"card-text\">".$value["description"]."</p>
                         <a href=\"".$value["lien"]."\" class=\"btn btn-moutarde mt-auto\">visiter</a>
                     </div>
