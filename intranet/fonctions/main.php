5b3c11196477ffdba579860790f46c5b4a3cb18e
ColletSimon <simoncollet2005@gmail.com>
2024-06-06 15:24:26 +0200

    partenaire dans un fichier a part

--- intranet/fonctions/main.php
+++ intranet/fonctions/main.php
@@ -9,6 +9,7 @@ require_once(__DIR__ . '/connexion.php');
 require_once(__DIR__ . '/general.php');
 require_once(__DIR__ . '/role.php');
 require_once(__DIR__ . '/collaborateur.php');
+require_once(__DIR__ . '/partenaire.php');
 require_once(__DIR__ . '/dashboard.php');
 require_once(__DIR__ . '/file-manager/repo-file.php');
 require_once(__DIR__ . '/file-manager/download.php');
