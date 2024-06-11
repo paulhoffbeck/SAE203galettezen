7c90683ca4937be2fbef1ddf94318f2148dd3490
TeoHubert <teohubert00@gmail.com>
2024-06-11 07:42:14 +0200

    Structure setting

--- intranet/settings.php
+++ intranet/settings.php
@@ -8,21 +8,29 @@
     <script src="js/bootstrap.bundle.min.js"></script>
     <link href="css/style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
-    <script type="text/javascript">
-        (function() {
-            emailjs.init("Yp4lst8YrH7O71nQi");
-        })();
-    </script>
 </head>
 <body class="d-flex flex-column min-vh-100 bg-pastel">
 
 <?php
 require_once('fonctions/main.php');
-head(); navbar();
+head(); navbar(); ?>
 
-footer();
-?>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-header">
+                    Personnes en ligne
+                </div>
+                <div class="card-body">
+                    <?= lastUserActivity() ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php footer(); ?>
 
 </body>
 </html>
