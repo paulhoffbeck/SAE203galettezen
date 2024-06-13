66e4757c6f828c86213de4009087a9ad08c39921
paulhoffbeck <paul.hoffbeck@etudiant.univ-rennes1.fr>
2024-06-14 00:08:56 +0200

    wiki

--- wiki/fonctions/navbar.php
+++ wiki/fonctions/navbar.php
@@ -48,6 +48,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="gestion-users.php">Gestion Membres</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="vitrine.php">Vitrine</a>
+                </li>
             </ul>
         </div>
     </div>
--- wiki/index.php
+++ wiki/index.php
@@ -92,6 +92,19 @@
                             </div>
                         </div>
                     </div>
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <img class="bd-placeholder-img card-img-top h-100" src="./img/vitrine.png" alt="" width="100%">
+                            <div class="card-body">
+                                <p class="card-text">Explorez les interfaces WEB du site vitrine !</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <a href="./vitrine.php" class="btn btn-sm btn-outline-secondary">Consulter</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
