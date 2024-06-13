4fde3bb713b9674c20969e06149998fe58fa430f
MaxymeDes <maxime.despres.1@etudiant.univ-rennes1.fr>
2024-06-13 21:33:29 +0200

    wiki page collaborateur

--- wiki/fonctions/navbar.php
+++ wiki/fonctions/navbar.php
@@ -36,6 +36,9 @@
                 <li class="nav-item">
                     <a class="nav-link" href="clients.php">Clients</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="collaborateur.php">Collaborateurs</a>
+                </li>
             </ul>
         </div>
     </div>
--- wiki/index.php
+++ wiki/index.php
@@ -66,6 +66,19 @@
                             </div>
                         </div>
                     </div>
+                    <div class="col">
+                        <div class="card shadow-sm">
+                            <img class="bd-placeholder-img card-img-top h-100" src="./img/user-card.png" alt="" width="100%">
+                            <div class="card-body">
+                                <p class="card-text">Découvrez comment nous avons réaliser la page d'affichage de nos employés/collaborateurs et toutes ses fonctionnalités.</p>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <div class="btn-group">
+                                        <a href="./collaborateur.php" class="btn btn-sm btn-outline-secondary">Consulter</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
